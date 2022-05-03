08483a9b074f7e29200976c03114af588db5dcbe
Everson Tomasi <everson.tomasi@gmail.com>
2022-05-02 23:31:16 -0300

    Criando teclado via JS

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 // Teclado
-function montaTeclado(){
-  const letrasTeclado = 'qwertyuiop asdfghjklç zxcvbnm'.split('');
+montaTeclado();
 
+function montaTeclado(){
+	const letrasTeclado = 'QWERTYUIOP ASDFGHJKLÇ ZXCVBNM'.split('');
+	const espacoTeclado = document.getElementById("teclado");
+    espacoTeclado.innerHTML = "";
+	
   for (let i = 0; i < letrasTeclado.length; i++) {
     if (letrasTeclado[i] === " ") {
-		document.write('<br>');
+		espacoTeclado.innerHTML = espacoTeclado.innerHTML + "<br>"
 	}	
     else {
-		document.write('<button type="button" value="' + letrasTeclado[i] + '">' + letrasTeclado[i] + '</button>');
+		espacoTeclado.innerHTML = espacoTeclado.innerHTML + "<button id=" + letrasTeclado[i] + " onclick=\"verificaTecla('" + letrasTeclado[i] + "')\">" + letrasTeclado[i] + "</button>"
     }
   }
 }
- 
+function verificaTecla(letra){
+  console.log(letra); 
+}
  
  
  
